55273697b63d6f087322279a53e203db3a968262
abhishek kumar majumdar <pk006604@gmail.com>
2024-04-15 17:17:02 +0530

    neet

--- src/components/PlayStoreLink.tsx
+++ src/components/PlayStoreLink.tsx
@@ -7,8 +7,6 @@ export function PlayStoreLink() {
   return (
     <Link
       href="https://play.google.com/store/apps/details?id=com.odyssey.meeting"
-      aria-label="Download on the App Store"
-      target="_blank"
     >
       <Image src={logoPlayStore} alt={"Play Store Logo"} />
     </Link>
